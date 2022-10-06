3fe21694a0843d1a531a590a1108c72e51f11856
ohyujeong <ohlife4@gmail.com>
2022-10-06 20:27:02 +0900

    first commit

---
+++ src/main/java/com/sm/expose/ExposeApplication.java
@@ -0,0 +1,13 @@
+package com.sm.expose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExposeApplication.class, args);
+	}
+
+}
